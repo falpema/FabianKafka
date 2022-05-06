678a7ef10f564463209f9f462d406f9386e2c97c
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-06 16:29:50 -0500

    Producer with Callbacks

--- src/main/java/com/falpema/kafka/callbacks/FalpemaCallbacks.java
+++ src/main/java/com/falpema/kafka/callbacks/FalpemaCallbacks.java
@@ -1,17 +1,22 @@
-package com.falpema.kafka.producers;
+package com.falpema.kafka.callbacks;
 
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class FalpemaProducer {
-
-	public static final Logger log = LoggerFactory.getLogger(FalpemaProducer.class);
+/**
+ * Producers con callbacks
+ * @author admin
+ *
+ */
+public class FalpemaCallbacks {
+	public static final Logger log = LoggerFactory.getLogger(FalpemaCallbacks.class);
 
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
@@ -23,23 +28,27 @@ public class FalpemaProducer {
 		props.put("linger.ms", "10");
 
 		try (Producer<String, String> producer = new KafkaProducer<>(props);) {
-			for (int i = 0; i < 1000000; i++) {
-				producer.send(new ProducerRecord<String, String>("devs4j-topic", String.valueOf(i), "devs4j-value"));
-				// el .get() me asegura que sea sincrono el envio de mensajes , es mas lento
+			for (int i = 0; i < 10000; i++) {
+				producer.send(new ProducerRecord<String, String>("devs4j-topic", String.valueOf(i), "devs4j-value"),
+						new Callback() {
+
+							// Evento que se dispara una vez entregado el mensaje
+							@Override
+							public void onCompletion(RecordMetadata metadata, Exception exception) {
+								if (exception != null) {
+									log.info("There was an error {} ", exception.getMessage());
+								} else {
+									log.info("Offset = {}, Partitions = {}, Topic = {}", metadata.offset(),
+											metadata.partition(), metadata.topic());
+								}
+
+							}
+						});
+
 			}
 			producer.flush();
-		} /*
-			 * catch (InterruptedException| ExecutionException e ) { e.printStackTrace();
-			 * log.error("Message produced interrumped", e); }
-			 */
+		}
 		log.info("Processing time = {} ms ", (System.currentTimeMillis() - startTime));
 
-		/**
-		 * PARAMETROS IMPORTANTES BATCH-SIZE -> Define tamaño de grupos de mensajes para
-		 * kafka BUFFER.MEMORY -> Tamaño maximo del buffer en el que se pueden colocar
-		 * los batches LINGER.MS -> Define cada cuanto ms se hace el envio de estos
-		 * mensajes
-		 */
 	}
-
 }
